693fecca64a660c15b98b73d82c131049b159972
ekimbasoglu <ekimbasoglu@hotmail.com>
2021-09-20 18:10:40 +0200

    (chore): Initial commit
    Finished the front end part, next is backend with Nodejs, and then learning&implementing to React

---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+const _ = require('lodash');
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.html'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        main: '#32445a',
+        twitter: '#1da1f2',
+        bordercolor: '#413f3f',
+        react: '#61dbfb',
+        tailwind: '#00b4b6',
+        nodejs: '#83ba63',
+        mongodb: '#4db33d',
+        aws: '#ff9900'
+      },
+      spacing: {
+        '15': '3.75rem'
+      },
+      zIndex: {
+        '-1': '-1',
+      },
+      borderWidth: {
+        '1': '1px'
+      }
+    },
+  },
+  variants: {
+    extend: {
+      listStyleType: ['square', 'inside']
+    },
+  },
+  plugins: [
+    plugin(function ({ addUtilities, theme, e }) {
+      const calcUtilities = _.map(theme('spacing'), (value, key) => {
+        return {
+          [`.${e(`calc-h-full-${key}`)}`]: {
+            height: `calc(100% - ${value})`
+          },
+          [`.${e(`calc-w-full-${key}`)}`]: {
+            width: `calc(100% - ${value})`
+          }
+        }
+      })
+
+      addUtilities(calcUtilities, {
+        variants: ['responsive', 'hover'],
+      })
+    })
+  ],
+}
