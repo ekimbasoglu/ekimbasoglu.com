73561c1a420e1e288e3ba76ca98a94f5a2c784c9
Ekim <ekimbasoglu@hotmail.com>
2023-06-13 18:28:33 +0200

    angular - removed & init again

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-tour-of-heroes'`, () => {
+  it(`should have as title 'angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-tour-of-heroes');
+    expect(app.title).toEqual('angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-tour-of-heroes app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular app is running!');
   });
 });
