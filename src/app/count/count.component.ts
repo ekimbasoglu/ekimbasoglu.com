a457e073c8b5552162b86bbf04907a0146dba1c2
Ekim <ekimbasoglu@hotmail.com>
2023-06-16 01:44:44 +0200

    Visitors counter - Init nodejs, angular part is done

---
+++ src/app/count/count.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-count',
+  templateUrl: './count.component.html',
+  styleUrls: ['./count.component.css'],
+})
+export class CountComponent {}
