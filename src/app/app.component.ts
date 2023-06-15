73bbebc022d442309e5816c260d60752af1a18d5
Ekim <ekimbasoglu@hotmail.com>
2023-06-16 00:49:54 +0200

    Dark mode updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  colorTheme = '';
   constructor() {
     if (localStorage.getItem('dark')) {
+      this.colorTheme = '[dark mode]';
       document.documentElement.classList.add('dark');
     }
   }
   darkMode() {
     document.documentElement.classList.toggle('dark');
     if (document.documentElement.classList.contains('dark')) {
+      this.colorTheme = '[light mode]';
       localStorage.setItem('dark', 'true');
     } else {
+      this.colorTheme = '[dark mode]';
       localStorage.removeItem('dark');
     }
   }
